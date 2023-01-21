0f3ba9c4d7e73b4c9ea441bc24be544bc5a85ae9
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2023-01-21 16:45:42 +0600

    update

--- index.js
+++ index.js
@@ -3,11 +3,53 @@ const app = express()
 const port = process.env.port || 9000
 
 
-const catagories =require('./database/catagories.json')
+const catagories =require('./database/catagories.json');
+const news =require('./database/news.json');
+
 
 app.get('/',(req, res)=>{
     res.send('Server is Running ... ')
+});
+
+app.get('/news-categories',(req,res)=>{
+    res.send(catagories)
+});
+
+app.get('/news',(req,res)=>{
+    res.send(news)
+});
+
+
+app.get('/news/:id',(req,res)=>{
+    // console.log(req.params)
+    // console.log(req.params.id)
+    const id =req.params.id;
+    const selectedNews =news.find(newsId=>newsId._id==id)
+    res.send(selectedNews);
+});
+
+app.get('/category/:id',(req, res)=>{
+    const id =req.params.id;
+
+    if(id === '08' ){
+        res.send(news)
+    }
+    else{
+        const category_news =news.filter(catId=>catId.category_id===id);
+         res.send(category_news);
+    }
+    
+
 })
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is  listening on port ${port}`)
